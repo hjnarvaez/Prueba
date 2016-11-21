0b8fc3d3b38fe716f7882974afc595a9fd8b9ec2
jonathan <jonathan@172.20.5.55>
2016-11-21 09:16:28 -0500

    vector

--- EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
+++ EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
@@ -3,16 +3,27 @@ package ejemplogithub;
 public class EjemploGitHub {
 
     public static void main(String[] args) {
-        EjemploGitHub objeto=new EjemploGitHub();
+        EjemploGitHub objeto = new EjemploGitHub();
         objeto.vector();
-        
+
+    }
+
+    private void vector() {
+        int A[], i;
+        int[] B = {1, 2, 3, 4};
+        A = new int[5];
+        i = 0;
+        do {
+            A[i] = i * 2;
+            i++;
+        } while (i < 5);
+        imprimir(A);
     }
     
-    private void vector(){
-        int A[];
-        int []B={1,2,3,4};
-        A=new int[5];
-        
+    public void imprimir(int[] A1){
+        int i=0;
+        while(i<A1.length){
+            System.out.print(A1[i]);
+        }
     }
-    
 }
